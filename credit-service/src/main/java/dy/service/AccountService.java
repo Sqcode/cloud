5402b21c55e4a504a8aaa35e82ac50fff23b7586
suqicun <570300991@qq.com>
2021-04-15 09:41:24 +0800

    rename

--- credit-service/src/main/java/dy/service/AccountService.java
+++
@@ -1,9 +0,0 @@
-package dy.service;
-
-public interface AccountService {
-
-    /**
-     * 从用户账户中借出
-     */
-    void debit(Integer userId, Integer money);
-}
---
+++ stock-service/src/main/java/sqc/controller/StockController.java
@@ -0,0 +1,25 @@
+package sqc.controller;
+
+import sqc.service.StockService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("stock")
+public class StockController {
+
+    @Autowired
+    private StockService stockService;
+
+    @GetMapping("/deduct/{id}/{num}")
+    public String deductStock (@PathVariable Integer id, @PathVariable Integer num) {
+        System.out.println("/stock/deduct");
+        stockService.deduct(id, num);// 减库存
+
+        return "success";
+    }
+
+}
