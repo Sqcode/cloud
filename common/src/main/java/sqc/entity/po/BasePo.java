d8b1620f7ca73e7c88c3dd5aeed4ab45d63c18b1
suqicun <570300991@qq.com>
2021-04-16 16:48:45 +0800

    add <module>common</module>

---
+++ common/src/main/java/sqc/entity/po/BasePo.java
@@ -0,0 +1,28 @@
+package sqc.entity.po;
+
+import com.baomidou.mybatisplus.annotation.FieldFill;
+import com.baomidou.mybatisplus.annotation.IdType;
+import com.baomidou.mybatisplus.annotation.TableField;
+import com.baomidou.mybatisplus.annotation.TableId;
+
+import java.io.Serializable;
+import java.util.Date;
+
+public class BasePo implements Serializable {
+
+    @TableId(type = IdType.ID_WORKER_STR)
+    private String id;
+
+    @TableField(fill = FieldFill.INSERT)
+    private String createdBy;
+
+    @TableField(fill = FieldFill.INSERT)
+    private Date createdTime;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private String updatedBy;
+
+    @TableField(fill = FieldFill.INSERT_UPDATE)
+    private Date updatedTime;
+
+}
