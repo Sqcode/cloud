f72d3c8ced52d54e8165e34ae6369c7ce78a9629
suqicun <570300991@qq.com>
2021-04-28 16:57:24 +0800

    add mybatis-plus CodeGeneration

--- common/src/main/java/sqc/entity/po/BasePO.java
+++ common/src/main/java/sqc/entity/po/BasePO.java
@@ -12,7 +12,7 @@ import java.util.Date;
 @Data
 public class BasePO implements Serializable {
 
-    @TableId(type = IdType.ID_WORKER_STR)
+    @TableId(type = IdType.ASSIGN_UUID)
     private String id;
 
     @TableField(fill = FieldFill.INSERT)
