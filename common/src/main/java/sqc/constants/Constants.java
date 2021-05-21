a7dba94be1dfe895d7b6e0a48e2956d815969af3
suqicun <570300991@qq.com>
2021-05-21 10:34:22 +0800

    exception errorType enum

---
+++ common/src/main/java/sqc/constants/Constants.java
@@ -0,0 +1,12 @@
+package sqc.constants;
+
+/**
+ * @Description:
+ * @Author: Sqcode
+ * @Date: 2021/5/21 10:30
+ */
+public class Constants {
+    final String A = "A";
+    final String B = "B";
+    final String C = "C";
+}
