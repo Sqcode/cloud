3a72eb7d42c8f97cba602c5b942401259ba10a0f
sqc <570300991@qq.com>
2021-08-18 13:33:55 +0800

    utf8

--- goods/src/main/java/sqc/goods/entity/param/GoodsParams.java
+++ goods/src/main/java/sqc/goods/entity/param/GoodsParams.java
@@ -3,6 +3,7 @@ package sqc.goods.entity.param;
 import io.swagger.annotations.ApiModelProperty;
 import lombok.Data;
 
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Pattern;
 
 @Data
---
+++ manage/src/main/java/cn/su/ManageApplication.java
@@ -0,0 +1,16 @@
+package cn.su;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @Description:
+ * @author: Sqcode
+ * @since: 2021/7/5 9:45
+ */
+@SpringBootApplication
+public class ManageApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(ManageApplication.class, args);
+    }
+}
